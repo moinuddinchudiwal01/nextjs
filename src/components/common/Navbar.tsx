87bc7332e0474ba5598a80e8a91df51baac2343b
Moinuddin <moinuddinchudiwal@gmail.com>
2025-10-31 18:33:38 +0530

    Navbar in progress

---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className='bg-background text-foreground border-b border-border'>
+      {/* desktop */}
+      <div></div>
+      {/* mobile */}
+      <div></div>
+    </nav>
+  );
+};
+
+export default Navbar;
